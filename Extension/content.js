01a556521c025cb1ddb308321898bd397fe16f14
Eric Tjon <ectjon@gmail.com>
2023-05-10 11:33:52 -0700

    Add content script

--- Extension/content.js
+++ Extension/content.js
@@ -1,1 +1,37 @@
 //content script
+console.log('hello world 4')
+
+if(document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded',afterDOMLoaded);
+} else {
+  afterDOMLoaded();
+}
+
+function afterDOMLoaded(){
+  //Everything that needs to happen after the DOM has initially loaded.
+  console.log('Dom Content loaded')
+  console.log(document.querySelector('#app').__vue_app__)
+  // const queryClient = document.getElementById('app').__vue_app__._context.app._context.provides.VUE_QUERY_CLIENT
+  // const queryCache = queryClient.getQueryCache()
+  // let startTime, endTime;
+  // const callback = (event) => {
+  //   if(event.type === 'added'){
+  //     startTime = Date.now();
+  //     console.log('API Query Started for', event.query.queryKey)
+  //   }
+  //   else if((event.type === 'updated' && event.action.type === 'success')){
+  //     if(startTime){
+  //       endTime = Date.now();
+  //       console.log('API Query Finished in', (endTime - startTime) / 1000 , 's for' , event.query.queryKey)
+  //       startTime = undefined;
+  //     } else {
+  //       console.log('Cache hit for' , event.query.queryKey)
+  //       //console.log(event)
+  //     }
+  //   } 
+  //   //console.log(event.type, event.query.observers[0]?.currentResult.status)
+  // }
+  // const unsubscribe = queryCache.subscribe(callback)
+}
+
+
