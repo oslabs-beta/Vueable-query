d90a63eddc8d2d86ed3118614cee7e84aa0efd9b
Eric Tjon <ectjon@gmail.com>
2023-05-09 19:41:13 -0700

    Clean up files

--- Extension/devtools.js
+++ Extension/devtools.js
@@ -1,21 +1,4 @@
-chrome.devtools.panels.create("Vueable Query", "icon.png", "index.html", panel => {
+chrome.devtools.panels.create("Vueable Query", "icon.png", "panel.html", panel => {
   // code invoked on panel creation
   console.log(panel);
 });
-
-// chrome.devtools.panels.create("Panel 2", "icon.png", "panel.html", function(panel){});
-
-// chrome.devtools.panels.create("My Panel", "icon.png", "index.html", function(panel) {
-//   panel.onShown.addListener(function(window) {
-//     var panelWindow = window;
-//     var panelDocument = panelWindow.document;
-
-//     var appFrame = panelDocument.createElement("iframe");
-//     appFrame.src = chrome.runtime.getURL("index.html");
-//     appFrame.style.width = "100%";
-//     appFrame.style.height = "100%";
-//     appFrame.style.border = "none";
-
-//     panelDocument.body.appendChild(appFrame);
-//   });
-// });
