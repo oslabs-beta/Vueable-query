22435314acc011402043f48c24f3a3975d76f963
Eric Tjon <ectjon@gmail.com>
2023-05-10 15:17:39 -0700

    Add injected script and content messages

--- Extension/content.js
+++ Extension/content.js
@@ -15,6 +15,24 @@ function afterDOMLoaded(){
   s.onload = function() { this.remove(); };
   (document.head || document.documentElement).appendChild(s);
 
+
+  window.addEventListener('message', function(event) {
+    // Only accept messages from the same frame
+    if (event.source !== window) {
+      return;
+    }
+  
+    var message = event.data;
+  
+    // Only accept messages that we know are ours
+    if (typeof message !== 'object' || message === null ||
+        message.source !== 'vueable-query-extension') {
+      return;
+    }
+    
+    console.log('content script heard window:', message);
+//    chrome.runtime.sendMessage(message);
+  });
 }
 
 
--- Extension/script.js
+++ Extension/script.js
@@ -1,22 +1,29 @@
 console.log('hello world!')
 const queryClient = document.getElementById('app').__vue_app__._context.app._context.provides.VUE_QUERY_CLIENT
 const queryCache = queryClient.getQueryCache()
+console.log(queryCache)
 let startTime, endTime;
 const callback = (event) => {
   if(event.type === 'added'){
     startTime = Date.now();
-    console.log('API Query Started for', event.query.queryKey)
+    // console.log('API Query Started for', event.query.queryKey)
+    messageWindow('API Query Started for' + event.query.queryKey.toString())
   }
   else if((event.type === 'updated' && event.action.type === 'success')){
     if(startTime){
       endTime = Date.now();
-      console.log('API Query Finished in', (endTime - startTime) / 1000 , 's for' , event.query.queryKey)
+      messageWindow(`API Query Finished in ${((endTime - startTime) / 1000).toString()} s for ${event.query.queryKey.toString()}`);
       startTime = undefined;
     } else {
-      console.log('Cache hit for' , event.query.queryKey)
-      //console.log(event)
+      messageWindow('Cache hit for ' +  event.query.queryKey.toString())
     }
   } 
-  //console.log(event.type, event.query.observers[0]?.currentResult.status)
 }
 const unsubscribe = queryCache.subscribe(callback)
+
+function messageWindow (message) {
+  window.postMessage({
+    message: message,
+    source: 'vueable-query-extension'
+  }, '*');
+}
