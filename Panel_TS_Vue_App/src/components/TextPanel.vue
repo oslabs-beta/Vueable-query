f7f5d69475c0b1cbc0be4d029aee2619718e6f3c
Eric Tjon <ectjon@gmail.com>
2023-05-09 19:40:43 -0700

    Convert Vue app to ts

---
+++ Panel_TS_Vue_App/src/components/TextPanel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="text-panel">
+    <h2>Text</h2>
+  </div>
+ 
+</template>
+
+<style scoped>
+  #text-panel {
+    height: 25rem;
+    width: 40rem;
+    border-style: solid
+  }
+</style>
