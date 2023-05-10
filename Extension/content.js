b8a35ca56bef14e49ff4990dfb029c5a7b7f3bb9
Eric Tjon <ectjon@gmail.com>
2023-05-10 11:39:51 -0700

    Add script.js to be injected into webpage

--- Extension/content.js
+++ Extension/content.js
@@ -8,30 +8,13 @@ if(document.readyState === 'loading') {
 }
 
 function afterDOMLoaded(){
-  //Everything that needs to happen after the DOM has initially loaded.
   console.log('Dom Content loaded')
-  console.log(document.querySelector('#app').__vue_app__)
-  // const queryClient = document.getElementById('app').__vue_app__._context.app._context.provides.VUE_QUERY_CLIENT
-  // const queryCache = queryClient.getQueryCache()
-  // let startTime, endTime;
-  // const callback = (event) => {
-  //   if(event.type === 'added'){
-  //     startTime = Date.now();
-  //     console.log('API Query Started for', event.query.queryKey)
-  //   }
-  //   else if((event.type === 'updated' && event.action.type === 'success')){
-  //     if(startTime){
-  //       endTime = Date.now();
-  //       console.log('API Query Finished in', (endTime - startTime) / 1000 , 's for' , event.query.queryKey)
-  //       startTime = undefined;
-  //     } else {
-  //       console.log('Cache hit for' , event.query.queryKey)
-  //       //console.log(event)
-  //     }
-  //   } 
-  //   //console.log(event.type, event.query.observers[0]?.currentResult.status)
-  // }
-  // const unsubscribe = queryCache.subscribe(callback)
+  
+  var s = document.createElement('script');
+  s.src = chrome.runtime.getURL('script.js');
+  s.onload = function() { this.remove(); };
+  (document.head || document.documentElement).appendChild(s);
+
 }
 
 
