2f86dce88aa2d2b6539e3b1ca00b665729f60c19
Esther Witbeck <esther.witbeck@gmail.com>
2023-05-16 15:44:31 -0700

    Tidy graph in timeline component
    shift graph to right and down;
    center each query in the band
    
    Co-authored-by: Anna Duong <annaduong94@users.noreply.github.com>
    Co-authored-by: Brandon Yoon <brand0n-y00n@users.noreply.github.com>
    Co-authored-by: ectjon <ectjon@users.noreply.github.com>

--- Panel_TS_Vue_App/src/components/Timeline.vue
+++ Panel_TS_Vue_App/src/components/Timeline.vue
@@ -1,24 +1,49 @@
 <script setup>
 import * as d3 from 'd3';
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted, watch, computed } from 'vue';
 import { useQueryStore } from '../store';
 
 const store = useQueryStore();
 
-const width = ref(500); // dynamic width and height
-const height = ref(250); // dynamic width and height
+//margins
+const margins = ref({
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 100
+});
 
+const rawWidth = ref(500); // dynamic width and height
+const rawHeight = ref(250); // dynamic width and height
+
+const width = computed(() => rawWidth.value - margins.value.left - margins.value.right)
+const height = computed(() => rawHeight.value - margins.value.top - margins.value.bottom)
+
+const queryHeight = 20;
 
 const refreshGraph = () => {
-    console.log('h and w are :', width.value, height.value)
+    // remove old graph
     d3.selectAll('.query').remove();
     d3.selectAll('.tick').remove();
     d3.selectAll('.domain').remove();
-    const svg = d3.select('#graph');
+    d3.selectAll('.graph').remove();
+    
+    // define new graph
+    const svg = d3.select('#graph')
+        .append('svg')
+        .attr('height', rawHeight.value)
+        .attr('width', rawWidth.value)
+        .classed('graph', true)
+        .append('g')
+        .attr('transform', `translate(${margins.value.left}, ${margins.value.top})`)
+        .classed('graph', true)
+        
+
     const x = d3.scaleLinear()
         .domain([0, store.lastEndTime])
-        .range([0, width.value]);
+        .range([margins.value.left, width.value]);
 
+    //y axis
     const y = d3.scaleBand()
         .domain(store.keys)
         .range([0, height.value]);
@@ -37,20 +62,27 @@ const refreshGraph = () => {
             return x(d.startTime);
         })
         .attr('y', function(d) {
-            return y(d.queryHash);
+            return y(d.queryHash) + y.bandwidth() / 2 - queryHeight / 2;
         })
         .attr('width', function(d) {
             return (x(d.endTime) - x(d.startTime)) || 2;
         })
         .classed('query', true)
-        .attr('height', 10)
+        .attr('height', queryHeight)
         .attr('fill', 'blue')
+
+        // .on({
+        //   "mouseover": function() { /* do stuff */ },
+        //   "mouseout":  function() { /* do stuff */ }, 
+        //   "click":  function() { /* do stuff */ }, 
+        // });
+        // change color based on q.originalIndex
 }
 
 watch(() => store.queries, refreshGraph)
 </script>
 
 <template>
-    
-    <svg id="graph" :width="width" :height="height"></svg>
+    <div id="graph"></div>
+<!-- <svg id="graph" :width="width" :height="height"></svg> -->
 </template>
