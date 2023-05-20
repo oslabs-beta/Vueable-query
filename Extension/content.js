b36fcb1c3c0aac6147726c412902ae58d7a5ed55
Esther Witbeck <esther.witbeck@gmail.com>
2023-05-20 15:30:57 -0700

    Reviews and commented codebase
    Co-authored-by: Anna Duong <annaduong94@users.noreply.github.com>
    Co-authored-by: Brandon Yoon <brand0n-y00n@users.noreply.github.com>
    Co-authored-by: ectjon <ectjon@users.noreply.github.com>

--- Extension/content.js
+++ Extension/content.js
@@ -1,4 +1,4 @@
-//content script
+//content script - runs every time Chrome Extension is opened; runs on every page 
 if(document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded',afterDOMLoaded);
 } else {
@@ -6,14 +6,17 @@ if(document.readyState === 'loading') {
 }
 
 function afterDOMLoaded(){
-  console.log('Dom Content loaded')
-  var script = document.createElement('script');
-  script.src = chrome.runtime.getURL('script.js');
-  script.onload = function() { this.remove(); };
-  (document.head || document.documentElement).appendChild(script);
+  // console.log('Dom Content loaded')
+  var script = document.createElement('script'); // creates script element on the document 
+  script.src = chrome.runtime.getURL('script.js'); // sets source to script.js; Chrome gives script.js a url 
+  script.onload = function() { this.remove(); }; 
+  (document.head || document.documentElement).appendChild(script); // append script to page
+  // (script runs on the page and is the only scrip that has access to queryCache)
 
 
-  //script.js -> here
+  
+  //script.js -> here 
+  //add a listener for messages from script.js
   window.addEventListener('message', function(event) {
     // Only accept messages from the same frame
     if (event.source !== window) {
@@ -27,9 +30,11 @@ function afterDOMLoaded(){
     }
     // forward message to background.js
     // console.log('content.js received from script.js:', message);
-    try{
-      chrome.runtime.sendMessage(message).catch(()=> console.log('Message has no response, please refresh the page'));
-    } catch{
+    try {
+      chrome.runtime.sendMessage(message).catch(() => { //catch error if theres no response after sending a message
+        console.log('Message has no response, please refresh the page');
+      });
+    } catch { // catch error if chrome.runtime can't be accessed
       console.log('Extension context invalidated, please refresh the page')
     }
   });
--- Panel_TS_Vue_App/src/components/TimelinePanel.vue
+++ Panel_TS_Vue_App/src/components/TimelinePanel.vue
@@ -1,31 +1,12 @@
 <script setup lang="ts">
 import { useQueryStore } from '../store';
 import Timeline from './Timeline.vue';
-  const store = useQueryStore();
 </script>
 
 <template>
   <div id="timeline-panel">
     <h2>Query Timeline</h2> 
-
-    <!-- <div
-      v-for="(queryList, queryHash) in store.groupedQueries"
-      >
-      {{ queryHash }}: {{ queryList }}
-    </div> -->
-    
     <Timeline />
-    <!-- <div 
-    v-for="q in store.queries"
-    :key="q.queryHash"
-    @click="store.setSelection(q.originalIndex)"
-    @mouseover="store.setHoverSelection(q.originalIndex)"
-    @mouseleave="store.setHoverSelection(-1)"
-    v-bind:style="q.originalIndex === store.selection ? {backgroundColor :'yellow'} : (q.originalIndex === store.hoverSelection ? {backgroundColor :'aqua'} : {})"        
-    class="query-timeline"
-    >
-      {{ q.queryHash }} {{  q.type }}: Started at {{ q.startTime }}ms
-    </div> -->
   </div>
  
 </template>
