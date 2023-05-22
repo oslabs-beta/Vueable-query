06c9ec149c01afbd4ea87679216e6851891f7b6c
Esther Witbeck <esther.witbeck@gmail.com>
2023-05-22 11:37:09 -0700

    Create multiple histories
    Create and verify each history synchronizes with respective tab.
    Histories do not impact other tabs. Light refactor/removal of
    console.logs for clarity.
    
    Includes bug: Pinia store appears to not properly reset any longer (we
    once again have negative start times and persisted query keys)

--- Extension/background.js
+++ Extension/background.js
@@ -1,14 +1,9 @@
 // content injects script to window, forwards messages to background.js, then background forwards them to devtool.js
 const connections = {};
-let history = [];
 const histories = {};
-let currentTabId;
 
 // when a tab closes, if it's the tab with active history, clear history
-chrome.tabs.onRemoved.addListener(
-  /* TODO refresh */
-  (tabId) => refreshHistoryConditionally(tabId, currentTabId)
-);
+chrome.tabs.onRemoved.addListener(refreshHistoryConditionally);
 
 /* when navigating to the page for any reason, if it's the tab with active */
 /* history, clear history; this conditional includes reload. */
@@ -17,68 +12,61 @@ chrome.tabs.onRemoved.addListener(
 chrome.webNavigation.onCommitted.addListener(
   ({ tabId, transitionType }) => {
     if (['reload'].includes(transitionType)) {
-    /* TODO refresh */
-      refreshHistoryConditionally(tabId, currentTabId)
+      refreshHistoryConditionally(tabId)
     }
   }
 );
 
 chrome.runtime.onConnect.addListener(function (port) { // listen for new ports
-    const extensionListener = function (message, sender, sendResponse) { // listen for messages
-      // The original connection event doesn't include the tab ID of the
-      // DevTools page, so we need to send it explicitly.
-      if (message.name == "init") {
-        currentTabId = message.tabId; //assign current tab Id to the tab Id vue query tool is in 
-        connections[currentTabId] = port;
-        /* TODO seelct appropriate history */
-        history.forEach((request) => {
-          connections[currentTabId].postMessage(request);
-        });
-        return;
-      }
-	// other message handling
+  const extensionListener = function ({ name, tabId }, sender, sendResponse) { // listen for messages
+    // The original connection event doesn't include the tab ID of the
+    // DevTools page, so we need to send it explicitly.
+    if (name === "init") {
+      connections[tabId] = port;
+      histories[tabId].forEach((request) => {
+        connections[tabId].postMessage(request);
+      });
+      return;
     }
-    // Listen to messages sent from the DevTools page
-    port.onMessage.addListener(extensionListener);
-    // handle disconnect
-    port.onDisconnect.addListener(function(port) {
-        port.onMessage.removeListener(extensionListener);
-        const tabs = Object.keys(connections);
-        for (const i = 0, length = tabs.length; i < length; i++) {
-          if (connections[tabs[i]] === port) {
-            delete connections[tabs[i]]
-            /* TODO delete history */
-            break;
-          }
+	// other message handling
+  }
+  // Listen to messages sent from the DevTools page
+  port.onMessage.addListener(extensionListener);
+  // handle disconnect
+  port.onDisconnect.addListener(function(port) {
+      port.onMessage.removeListener(extensionListener);
+      const tabs = Object.keys(connections);
+      for (const i = 0, length = tabs.length; i < length; i++) {
+        if (connections[tabs[i]] === port) {
+          delete connections[tabs[i]]
+          break;
         }
-    });
+      }
+  });
 });
 
 // Receive message from content script and relay to the devTools page for the current tab
 // content.js -> here -> devtool panel
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // Messages from content scripts should have sender.tab set
-  /* TODO specific history push */
-    if (!sender.tab) console.log ('something has gone very wrong');
-    history.push(request);
-    if (sender.tab) {
-      console.log('tab is in:', sender.tab);
-      const tabId = sender.tab.id;
-      if (tabId in connections) {
-//        console.log("Background.js: posting message to devtool panel", history);
-        connections[tabId].postMessage(request);
-      } else {
-        console.log("Tab not found in connection list.");
-      }
+  // Messages from content scripts should have sender.tab set
+  if (sender.tab) {
+    const tabId = sender.tab.id;
+    if (!histories.hasOwnProperty(tabId)) histories[tabId] = [];
+    histories[tabId].push(request);
+    if (tabId in connections) {
+      connections[tabId].postMessage(request);
     } else {
-      console.log("sender.tab not defined.");
+      console.log("Tab not found in connection list.");
     }
-    return true;
+  } else {
+    console.log("sender.tab not defined.");
+  }
+  return true;
 });
 
-function refreshHistoryConditionally (tabId, currentTabId) {
-  if (tabId === currentTabId) {
-    history = [];
+function refreshHistoryConditionally (tabId) {
+  if (Object.hasOwnProperty(tabId)) {
+    histories[tabId] = [];
     // send message to devTool store to reset
     // could also reset startTime
     connections[tabId].postMessage({
