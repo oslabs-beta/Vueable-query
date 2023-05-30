1d1246159c0d3374963cb826998e6846bba5f494
Eric Tjon <ectjon@gmail.com>
2023-05-30 15:20:22 -0700

    Add query cache hit tests

--- __tests__/QueryCacheHit.spec.ts
+++ __tests__/QueryCacheHit.spec.ts
@@ -1,21 +1,19 @@
 import { describe, it, expect } from 'vitest'
 import { VueWrapper, mount } from '@vue/test-utils'
-import QueryEnd from '../Panel/src/components/QueryEnd.vue'
+import QueryCacheHit from '../Panel/src/components/QueryCacheHit.vue'
 import { createTestingPinia } from '@pinia/testing'
 import { useQueryStore } from '../Panel/src/store';
 import fs from 'fs';
 import path from 'path';
-import { VueDd } from 'vue-dd';
 
-describe('Query End Vue Component', () => {
-
+describe('Query Cache Hit Vue Component', () => {
 
   let initialData: [Message];
   let wrapper: VueWrapper;
   let store: any;
   describe('Testing Setup', () => {
     it('imports the component properly', () => {
-      expect(QueryEnd).toBeTruthy();
+      expect(QueryCacheHit).toBeTruthy();
     })
     it('mounts properly with initialized store in testing environment', () => {
       // sets initialData to data.json, which is an array of Messages
@@ -29,8 +27,8 @@ describe('Query End Vue Component', () => {
       });
       store = useQueryStore();
       expect(store.data.length).not.toBe(0);
-      wrapper = mount(QueryEnd, {
-        props: {c: store.endQueries[0]},
+      wrapper = mount(QueryCacheHit, {
+        props: {c: store.cacheQueries[0]},
         global: {
           plugins: [
             testingPinia,
@@ -41,13 +39,10 @@ describe('Query End Vue Component', () => {
   })
   describe('displays', () => {
     it('the query hash', () => {
-      expect(wrapper.text()).toContain(store.endQueries[0].queryHash)
+      expect(wrapper.text()).toContain(store.cacheQueries[0].queryHash)
     })
     it('the start time', () => {
-      expect(wrapper.text()).toContain(store.endQueries[0].startTime)
-    })
-    it('a vue-dd component', () => {
-      expect(wrapper.findAllComponents(VueDd).length).toBe(1)
+      expect(wrapper.text()).toContain(store.cacheQueries[0].startTime)
     })
   })
 })
